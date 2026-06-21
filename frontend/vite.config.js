1436900352bd9857be038a328b908524915e379c
Nur Azhim Ramadhan <nurazhimrr@gmail.com>
2026-06-21 17:48:01 +0700

    fix github pages path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/portfolio-v2/',
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
